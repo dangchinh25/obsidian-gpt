2ee2c0b5b456dce3a8a96c82864e528ea2922afe
dangchinh25 <dangchinh2501@gmail.com>
2023-10-07 21:05:48 -0500

    Test obsidian view

--- main.ts
+++ main.ts
@@ -1,11 +1,34 @@
 import { Plugin } from 'obsidian';
+import { ExampleView, VIEW_TYPE_EXAMPLE } from './view';
 
 export default class ObsidianGPT extends Plugin {
     async onload (): Promise<void> {
+        this.registerView(
+            VIEW_TYPE_EXAMPLE,
+            ( leaf ) => new ExampleView( leaf )
+        );
 
+        this.addRibbonIcon( 'message-circle', 'Obsidian GPT', () => {
+            console.log( 'Obsidian GPT icon' );
+
+            this.activateView();
+        } );
     }
 
     onunload (): void {
 
+    }
+
+    async activateView () {
+        this.app.workspace.detachLeavesOfType( VIEW_TYPE_EXAMPLE );
+
+        await this.app.workspace.getRightLeaf( false ).setViewState( {
+            type: VIEW_TYPE_EXAMPLE,
+            active: true
+        } );
+
+        this.app.workspace.revealLeaf(
+            this.app.workspace.getLeavesOfType( VIEW_TYPE_EXAMPLE )[ 0 ]
+        );
     }
 }
