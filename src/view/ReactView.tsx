ea35a77fdd3e3f37963503a12679062b1fbb6ba7
dangchinh25 <dangchinh2501@gmail.com>
2023-10-08 11:47:16 -0500

    Simple QA view

--- src/view/ReactView.tsx
+++ src/view/ReactView.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ReactView(): JSX.Element {
   const styles = {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: '5px',
+    },
     submitButton: {
-      backgroundColor: 'white',
+      width: '75px',
+      alignSelf: 'end',
+    },
+    input: {
+      width: '100%',
+      height: '50px',
+      padding: '5px',
+      paddingLeft: '10px',
+      marginBottom: '10px',
+      borderRadius: '5px',
+      border: '1px solid black',
     },
   };
 
+  const [userInput, setUserInput] = useState<string>('');
+
+  const onClickSubmit = (e) => {
+    console.log(userInput);
+  };
+
   return (
-    <div>
-      <input />
-      <button style={styles.submitButton}>Submit</button>
+    <div style={styles.container}>
+      <input
+        style={styles.input}
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button style={styles.submitButton} onClick={onClickSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
